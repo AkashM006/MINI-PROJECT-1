0a7696a8783361878c12df173d26e6d45b0925f9
Akash M <akashm006@outlook.com>
2022-08-11 18:08:42 +0530

    Finished project, need to check for ay bugs

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,10 @@
 const app = require('express').Router();
 const passport = require('passport');
+const { genHash } = require('../config/utils');
 const User = require('../models/user');
 
 app.get("/", passport.authenticate("jwt", { session: false }), async (req, res) => {
     const user = await req.user;
-    console.log(user);
     const currentUser = {
         name: user.name,
         email: user.email,
@@ -14,6 +14,31 @@ app.get("/", passport.authenticate("jwt", { session: false }), async (req, res) 
         success: true,
         msg: currentUser
     })
+});
+
+app.post("/", passport.authenticate("jwt", { session: false }), async (req, res) => {
+    const user = await req.user;
+    let { name, password, email } = req.body.data;
+    try {
+        const userObject = await User.findOne({ where: { id: user.id } });
+        userObject.name = name;
+        userObject.email = email;
+        if (password !== '') {
+            password = await genHash(password);
+            userObject.password = password;
+        }
+        userObject.save();
+        return res.json({
+            success: true,
+            msg: "Successfully updated!"
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({
+            success: false,
+            msg: "Something went wrong!"
+        })
+    }
 })
 
 module.exports = app;
